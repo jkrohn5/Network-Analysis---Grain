a27b633b257ee1058a3e3ecb7531aa235e603f2a
jkrohn5 <64023511+jkrohn5@users.noreply.github.com>
2023-11-10 10:56:48 -0600

    Update Edge_Road.sql

--- Edge_Road.sql
+++ Edge_Road.sql
@@ -1,4 +1,4 @@
-drop table if exists CARES2SQL.CARES2SQL.Justin_LoopEdges_Road
+drop table if exists [DB].[Schema].Justin_LoopEdges_Road
 select CAST(RouteID as bigint) as RouteID, sum(cast(Attr_Length as float)) as Miles, left(Analysis_Date,10) as Analysis_Date, Type
-into CARES2SQL.CARES2SQL.Justin_LoopEdges_Road
-from CARES2SQL.CARES2SQL.Justin_LoopEdges where SourceName LIKE '%Road%' Group by CAST(RouteID as bigint), left(Analysis_Date,10), Type
+into [DB].[Schema].Justin_LoopEdges_Road
+from [DB].[Schema].Justin_LoopEdges where SourceName LIKE '%Road%' Group by CAST(RouteID as bigint), left(Analysis_Date,10), Type
