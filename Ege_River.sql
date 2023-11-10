30a1e5a51d09a1520b4ceed0a9c09cc23447f625
jkrohn5 <64023511+jkrohn5@users.noreply.github.com>
2023-11-10 10:56:10 -0600

    Update Ege_River.sql

--- Ege_River.sql
+++ Ege_River.sql
@@ -1,5 +1,5 @@
-Drop table if exists CARES2SQL.CARES2SQL.Justin_LoopEdges_River_Loop
+Drop table if exists [DB].[Schema].Justin_LoopEdges_River_Loop
 
 select CAST(RouteID as bigint) as RouteID, sum(cast(Attr_Length as float)) as Miles , left(replace(Analysis_Date,'"',''),10)  as Analysis_Date, Type
-into CARES2SQL.CARES2SQL.Justin_LoopEdges_River_Loop
-from CARES2SQL.CARES2SQL.Justin_LoopEdges where SourceName LIKE '%River%' Group by CAST(RouteID as bigint), left(replace(Analysis_Date,'"',''),10), Type
+into [DB].[Schema].Justin_LoopEdges_River_Loop
+from [DB].[Schema].Justin_LoopEdges where SourceName LIKE '%River%' Group by CAST(RouteID as bigint), left(replace(Analysis_Date,'"',''),10), Type
